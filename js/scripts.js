f8c1926d6ea34425b9abe5d80003c76a3a8771b4
Kevin Finley and Luke Philips <kevin@>
2016-10-18 11:15:28 -0700

    add a dark mode button

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+// $(document).ready(function() {
+//   // $("button.dark").click(function() {
+//   //   $("body").removeClass();
+//   //   $("body").addClass("dark");
+//   // });
+// });
+
+$(document).ready(function() {
+  $(".walrus").click(function() {
+    $("#hidden").toggle();
+    $("#showing").toggle();
+  });
+  $("span.carpenter").click(function() {
+    $("img#carpenter").fadeToggle("slow");
+  });
+  $("span#slider").click(function() {
+    $("img#sliding").slideToggle("slow");
+  });
+  $("button.red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red");
+  });
+  $("button.yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow");
+  });
+  $("button.blue").click(function() {
+    $("body").removeClass();
+    $("body").addClass("blue");
+  });
+  $("button.pink").click(function() {
+    $("body").removeClass();
+    $("body").addClass("pink");
+  });
+  $("button.default").click(function() {
+    $("body").removeClass();
+  });
+});
